06948da1e2f6dfa4192de787c4526dc55930663c
JewishBastard <marcinkowski.paw@gmail.com>
2013-04-02 22:06:02 +0200

    Mapki i pare poprawek

--- scripts_R/Maps.R
+++ scripts_R/Maps.R
@@ -1,22 +1,40 @@
-mapa1<-readShapePoly("G:/Skrypty/Sweave/Projekt 2/R_and_SAS_project_II/data/POL_adm/POL_adm1.shp")
+mapa<-readShapePoly("./data/POL_adm/POL_adm1.shp")
 
-dane$wojewodztwo <- factor(dane$wojewodztwo)
-dane$wojewodztwo <- factor(dane$wojewodztwo, levels = sort(levels(dane$wojewodztwo)))
-levels(dane$wojewodztwo) <- sort(levels(mapa$VARNAME_1))
+Data$Region <- factor(Data$Region, levels = sort(levels(Data$Region)))
+levels(Data$Region) <- sort(levels(mapa$VARNAME_1))
 dane$wojewodztwo <- factor(dane$wojewodztwo, levels = levels(mapa$VARNAME_1))
 
-sr.dochody <- sapply(levels(dane$wojewodztwo), function(x) mean(dane$dochod[dane$wojewodztwo == x]))
-sr.dochody.spodziewane <- sapply(levels(dane$wojewodztwo), function(x) mean(dane$spodziewany_dochod[dane$wojewodztwo == x]))
-roznica <- round(sr.dochody.spodziewane - sr.dochody)
+Poland_Heatmap <- function(variable,title,num_breaks,prec=0) {
 
-mapa$roznica <- sapply(mapa$VARNAME_1, function(x) roznica[x])
+  mean.variable <- sapply(levels(Data$Region), function(x) mean(variable[Data$Region == x]))
+  
+  mapa$mean.variable <- sapply(mapa$VARNAME_1, function(x) mean.variable[x])
+  
+  if(prec>0){
+    min_var <- round(min(mean.variable),digits=prec)
+    max_var <- round(max(mean.variable),digits=prec)
+    
+  }
+  else{
+    min_var <- floor(min(mean.variable))
+    max_var <- ceiling(max(mean.variable))
+  }
+  breaks <- round(seq(min_var, max_var,(max_var-min_var)/num_breaks),digits=prec)
+  
+  colours <- brewer.pal(num_breaks, "Blues")
+  
+  map_legend <- leglabs(breaks)
+  
+  par(xpd=TRUE)
+  plot(mapa, col=colours[findInterval(mapa$mean.variable, breaks, all.inside=TRUE)], axes=F, bty='L')
+  legend(x="bottom", inset=0, legend=map_legend, 
+         fill=colours, cex=0.7, title=title, horiz=TRUE)
+}
 
-brks <- c(min(roznica), 100, 500, 1000, max(roznica))
-kolory <- brewer.pal(4, "Blues")
+Poland_Heatmap(Data$Income,"Average Income in PLN",5)
 
-lglbls <- gsub("under", "pon.", leglabs(brks))
-lglbls <- gsub("over", "pow.", lglbls)
+Poland_Heatmap(Data$Unemployment_Frequency,"Average frequency of unemployment",4,2)
 
-par(xpd=TRUE, mar=c(5,3,2,3))
-plot(mapa, col=kolory[findInterval(mapa$roznica, brks, all.inside=TRUE)], axes=F, bty='L')
-legend(x="bottom", inset=0, legend=lglbls, fill=kolory, cex=0.7, title="Różnica pomiędzy spodziewanym a osiaganym dochodem w PLN", horiz=TRUE)
+Poland_Heatmap(Data$Unemployment_Spells,"Average unemployment duration in months",4,1)
+
+Poland_Heatmap(as.numeric(Data$Is_Unemployed)-1,"Average frequency of unemployment",3,4)
