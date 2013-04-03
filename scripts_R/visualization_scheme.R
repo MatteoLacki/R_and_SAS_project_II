b578fc9443dd74030eccee2f14018b40596aa851
JewishBastard <marcinkowski.paw@gmail.com>
2013-04-03 04:36:10 +0200

    schemat wizualizacyjny

---
+++ scripts_R/visualization_scheme.R
@@ -0,0 +1,30 @@
+visualsation_scheme <- function(Set){ 
+
+  par(mfrow=c(2,2))
+Poland_Heatmap(as.numeric(Set$Is_Unemployed)-1,"Average frequency of unemployment",3,4)
+
+
+slices_sex <- c(table(Set$Sex))
+lbls_sex <- paste(levels(Set$Sex),"\n",slices_sex,sep="")
+
+pie3D(table(Set$Sex),labels=lbls_sex,theta=0.6,labelcex=1,start=0.5,col=c("blue","green"),explode=0.1,main=" 3D Pie Chart of Sex ")
+
+slices_eng <- c(table(Set$Speaks_English))
+lbls_eng <- paste(levels(Set$Speaks_English),"\n",slices_eng,sep="")
+
+pie3D(slices_eng,labels=lbls_eng,theta=0.6,labelcex=1,start=0.5,col=c("blue","green","white"),explode=0.1,main=" 3D Pie Chart of Knowlage of English ")
+Histogram_z_wypełnieniem(Data$Speaks_English, Data$Is_Unemployed, Data)
+
+}
+z <- visualsation_scheme(Data)
+cond_visualization_scheme <- function(set,var){
+levels_var <- levels(var)
+sapply(1:length(levels_var),function(x)
+  {
+    sapply(1:length(var),function(y) {
+                                    ifelse(var)
+    }
+                                    )
+   
+   })  
+}
